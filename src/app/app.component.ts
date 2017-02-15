f576e75be0c4431e4a9ae8e82e563939179f6e6c
Ryan Fleming <fleming.rp@gmail.com>
2017-02-15 13:34:27 -0500

    Lab: The basic Observable Sequence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +14,11 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   @ViewChild('btn') btn;
-  message: string;
+  message = 'Hello RxJS!';
 
   ngOnInit() {
+    Observable.fromEvent(this.getNativeElement(this.btn), 'click')
+      .subscribe(event => this.message = 'Beast Mode Activated!');
   }
 
   getNativeElement(element) {
